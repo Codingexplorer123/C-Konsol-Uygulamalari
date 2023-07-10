ee00c3d5334a144f0e6834ca59fe59c6b3a401b5
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-11 01:13:02 +0300

    Add files via upload

---
+++ Ortalama Hesaplama/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Ortalama_Hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonanci sayisilari dizisi nereye kadar olsun");
+            int derinlik = Convert.ToInt32(Console.ReadLine());
+            
+            FibonanciSerisi(derinlik);
+
+
+
+
+
+        }
+        public static void FibonanciSerisi(int derinlik)
+        {
+            Console.WriteLine("Fibonacci Series:");
+
+            int sayi1 = 0;
+            int sayi2 = 1;
+            int sayi3 = 0;
+            double[] sayilar = new double[derinlik];
+            
+
+            for (int i = 0; i < derinlik; i++)
+            {
+                Console.WriteLine(sayi1);
+                
+
+                sayi3 = sayi1 + sayi2;
+                sayi1 = sayi2;
+                sayi2 = sayi3;
+                sayilar[i] = sayi1; 
+            }
+            double ortalama = 0;
+            foreach (var sayi in sayilar)
+            {
+                ortalama += sayi / derinlik;
+            }
+            Console.WriteLine("Fibonanci sayilarin ortalamasi :" + ortalama);
+
+
+        }
+        
+    }
+    
+}
