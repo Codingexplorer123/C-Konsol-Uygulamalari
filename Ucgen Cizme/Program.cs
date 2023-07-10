dda298588de51fc89f7f4d992ea9fef416efb402
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-11 01:43:26 +0300

    Add files via upload

---
+++ Ucgen Cizme/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ucgen_Cizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ucgenin kisa kenarlarinin boyutunu pozitif tam sayi olarak giriniz");
+            int uzunluk = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < uzunluk; i++)
+            {
+                
+                for (int j = 0; j < uzunluk; j++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("*");
+                    }
+                    
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
