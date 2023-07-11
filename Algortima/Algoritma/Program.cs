e3452e7964688b0e2b0170d062d26a47ff5cb9c5
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-11 14:57:44 +0300

    Add files via upload

---
+++ Algortima/Algoritma/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime yaziniz");
+            string kelime = Console.ReadLine();
+            Console.WriteLine("index numarasi giriniz");
+            int index = Convert.ToInt32(Console.ReadLine());
+            Algoritma(kelime, index);
+
+        }
+        public static void Algoritma(string kelime, int index)
+        {
+
+            var kelimelist = kelime.ToList();
+            try
+            {
+                kelimelist.RemoveAt(index);
+                // RemoveAt o index deki listede bulunan degeri siler.
+                foreach (var item in kelimelist)
+                {
+                    Console.Write(item);
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine(kelime);
+            }
+           
+        }  
+
+ 
+        
+    }
+}
