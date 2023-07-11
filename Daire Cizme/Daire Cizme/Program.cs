9ea57494ade3fbd3052f3594f7b61694707eba34
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-11 13:30:34 +0300

    Add files via upload

---
+++ Daire Cizme/Daire Cizme/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Daire_Cizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dairenin Yaricapi degerini pozitif tam sayi olarak giriniz");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            DaireCizme(radius);
+        }
+        public static void DaireCizme(int radius)
+        {
+            
+            for (int y = -radius; y <= radius; y++)
+            {
+                for (int x = -radius; x <= radius; x++)
+                {
+                    if (x * x + y * y <= radius * radius)
+                    {
+                        Console.Write("*"); // Dairenin içi
+                    }
+                    else
+                    {
+                        Console.Write("d "); // Dairenin dışı
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
