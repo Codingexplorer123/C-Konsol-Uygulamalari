4398a1e7ca5cb7cabbf83e56a4b451bacc194306
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-11 15:21:06 +0300

    Add files via upload

---
+++ Karakter Testen Yazdirma/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Karakter_Testen_Yazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime giriniz");
+            string kelime = Console.ReadLine();
+            TersYazma(kelime);
+        }
+        static void TersYazma(string kelime)
+        {
+            var kelimelist = kelime.ToList();
+            
+            for (int i = 0; i<kelime.Length-1; i++)
+            {
+                var gecicidegisken = kelimelist[i];
+                kelimelist[i] = kelime[i + 1];
+                kelimelist[i+1] = gecicidegisken;
+                if(i -1 == kelime.Length)
+                {
+                    gecicidegisken = kelimelist[kelime.Length-1];
+                    kelimelist[i + 1] = kelimelist[0];
+                    kelimelist[i] = gecicidegisken;
+                }
+
+            }
+            
+            foreach (var harf in kelimelist)
+            {
+                Console.Write(harf);
+            }
+        }
+    }
+}
