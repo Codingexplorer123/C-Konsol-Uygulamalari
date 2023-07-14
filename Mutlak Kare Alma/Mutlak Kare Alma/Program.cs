97ba18801d89a7d0bddd0e106428e85846533958
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-15 00:44:32 +0300

    Add files via upload

---
+++ Mutlak Kare Alma/Mutlak Kare Alma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace Mutlak_Kare_Alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consoldan girmek istediginiz sayi adedini belirtiniz");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            List<int> sayilistesi = new List<int>();
+            int kucuktoplam = 0;
+            double buyuktoplam = 0;
+            for (int i = 0; i < adet; i++)
+            {
+                Console.WriteLine($"{i + 1} sayiyi giriniz.");
+                sayilistesi.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            foreach (int i in sayilistesi)
+            {
+                if (i <= 67)
+                {
+                   kucuktoplam += (67-i);
+                }
+                else
+                {
+                    buyuktoplam += Math.Pow((i - 67),2);
+                }
+            }
+            Console.WriteLine($"Output: {kucuktoplam} ve {buyuktoplam}");
+        }
+    }
+}
