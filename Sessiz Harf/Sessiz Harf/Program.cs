de7805cc4525dd9b3bf862438d5dd6fe158f8112
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-07-15 08:16:24 +0300

    Add files via upload

---
+++ Sessiz Harf/Sessiz Harf/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sessiz_Harf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime yaziniz ");
+            string kelime = Console.ReadLine();
+            
+            SessizHarfKontrolu(kelime);
+           
+
+        }
+        public static void  SessizHarfKontrolu( string kelime)
+        {
+            string sessizHarfler = "bcdfghjklmnprstvyz";
+            int sayac = 0;
+            for (int i = 0; i < kelime.Length-1; i++)
+            {
+                if (sessizHarfler.Contains(kelime[i]) && sessizHarfler.Contains( kelime[i+1]))
+                {
+                    sayac++;
+                   
+                }
+                
+            }
+            if (sayac > 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
